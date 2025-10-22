3a6bbf5fb533a40e29484a151220568fccd7e446
raworiginal <118763950+raworiginal@users.noreply.github.com>
2025-10-22 10:06:32 -0700

    Created basic express server with a healthcheck

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//middleware to parse JSON
+app.use(express.json());
+
+app.get("/health", (req: Request, res: Response) => {
+	res.json({ status: "ok", message: `Server is running on port ${PORT}` });
+});
+
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
